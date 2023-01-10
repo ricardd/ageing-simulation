b41373c913857f6fce0b5dc4bb85a2346654c9dd
ricardd <daniel.ricard@gmail.com>
2023-01-10 16:32:54 -0400

    add work on simulations

--- simulating-observations/simulating-observations.R
+++ simulating-observations/simulating-observations.R
@@ -18,9 +18,9 @@ lw.fct <- function(length, a=0.001,b=3.1){
   return(a*(length^b))
 }
 
-simulate.pop.fct <- function(m=0.2){
+simulate.pop.fct <- function(seed=1973, m=0.2){
 
-  set.seed(1973)
+  set.seed(seed)
 
   ages <- 0:20
   years <- 1:120
@@ -58,6 +58,7 @@ simulate.pop.fct <- function(m=0.2){
     this.n <- round(yearly.df[i] * this.q)
     t.df <- data.frame(year=years[i], n.captured=this.n)
     obs.df <- rbind(obs.df,t.df)
+
     ## for the individuals captured, assign an age based on the
     ## age structure in the simulated population
     t2.df <- data.frame(year=years[i], fish.no=1:this.n)
@@ -82,45 +83,78 @@ simulate.pop.fct <- function(m=0.2){
 
 
 
-l <- simulate.pop.fct(m=0.5)
+l <- simulate.pop.fct(seed=1973, m=0.5)
 x <- l[[1]]
 
-pal.fire=c(	rgb(255/255, 246/255, 143/255, 1),
-            rgb(255/255, 215/255, 0/255, 1),
-            rgb(255/255, 100/255, 2/255, 1),
-            rgb(221/255, 8/255, 6/255, 1),
-            rgb(139/255, 0/255, 0/255, 1))
+library(RColorBrewer)
+pal.fire <- brewer.pal(9, "OrRd")
 
-br <- c(0, 1E04, 1E05, 1E06, 1E07, 1E08, 1E09)
+# pal.fire=c(	rgb(255/255, 246/255, 143/255, 1),
+#             rgb(255/255, 215/255, 0/255, 1),
+#             rgb(255/255, 100/255, 2/255, 1),
+#             rgb(221/255, 8/255, 6/255, 1),
+#             rgb(139/255, 0/255, 0/255, 1))
+
+br <- c(0, 1E03, 1E04, 1E05, 1E06, 5E06, 1E07, 5E07, 1E08, 5E08, 1E09)
 
 image(x=0:100, y=0:21, z=t(x), col=c("white",pal.fire), breaks=br, xlab="", ylab="", axes=FALSE)
 
-grid()
+#grid()
 box()
-axis(side=1, at=seq(1,100,10), padj=-1)
+axis(side=1, at=seq(1,100,10)-0.5, labels=seq(1,100,10), padj=-1)
 axis(side=1, at=1:100, labels = FALSE, tck=-0.01)
-axis(side=2, at=seq(0,21), padj=1)
+axis(side=2, at=seq(0,21)+0.5, labels=seq(1,22), padj=1)
+for(i in seq(-19,101,5)){
+  abline(a=-i,b=1, lty=2, col=grey(0.6))
+}
+
 
 ## total number of individuals per year
+dev.new()
 plot(colSums(x), type='b', pch=19, ylim=c(0, max(colSums(x))))
-par(new=TRUE)
+
+
 y <- l[[2]]
-plot(y, type='b', pch=19)
 
 cor(colSums(x),y$n.captured)
 
 range(y$n.captured)
 
+
+## biomass per year
+ages <- 0:20
+years <- 1:100
+waa.mat <- matrix(rep(vb.fct(ages), length(years)), nr=length(ages),byrow=FALSE)
+
+dim(x) == dim(waa.mat)
+y <- x * waa.mat
+
+plot(colSums(x), type='b', pch=19, ylim=c(0, max(colSums(x))), col="red")
+par(new=TRUE)
+plot(colSums(y), type='b', pch=19, ylim=c(0, max(colSums(y))), col="blue")
+legend("topright", c("N","B"), pch=19, lty=1, col=c("red","blue"))
+
+
 ## total weight of individuals per year
-
-
-
 z <- l[[3]]
-
-
 head(z)
 agg.df <- aggregate(fish.no~age+year, data=z, length)
 agg.df[agg.df$year==21,]
 idx <- sample(1:nrow(z),1000)
 plot(z[idx,"age"], z[idx,"length"])
 plot(z[idx,"length"], z[idx,"weight"])
+
+## now sample age-length pairs from the simulated population
+## and try to fit a VB model
+
+
+## obtain growth trajectories and fit a VB model
+
+
+
+## compute the age-length key for the sampled individuals
+
+## compute the catch-at-age matrix obtained from the sampled individuals
+
+## compare the catch-at-age matrix to the true catch-at-age matrix
+
